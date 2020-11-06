42887e286ce20cecd196c23df7764e944f2dd90c
nicoleooi <nicoleooi@hotmail.com>
2020-11-05 21:05:37 -0500

    reformatted hr files, added script to extract hr data

--- scripts/HR_cleaner.py
+++ scripts/HR_cleaner.py
@@ -7,10 +7,13 @@ dirs = os.listdir(path) #get all directories
 for x in dirs:
     if x.find(".") != -1: #no files 
         pass
-    print(path+x)
     f = os.listdir(path+x)
     for i in f:
-        df = pd.read_csv(path+x+f)
-    # delete blank rows
-    # write to CSV
+        df = pd.read_csv(path+x+"/"+i)
+        hr = df[["timestamp", "heart_rate"]].dropna()
+        name = i.replace(".csv", "")
+        fullpath=path+"hr_only/"+name+"_hr.csv"
+        hr.to_csv(fullpath, index=False)
+        #will not work fully for person C bc not same format yet 
+        print(fullpath)
 
