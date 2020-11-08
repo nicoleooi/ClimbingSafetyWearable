c8038ff8494a6b83065414d12c2dea6719b1b95e
nicoleooi <nicoleooi@hotmail.com>
2020-11-08 15:12:14 -0500

    started example LSTM

---
+++ scripts/LSTMexample.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  8 15:09:52 2020
+
+@author: Nicole Ooi and Alex Ingham
+"""
+
+import numpy
+import keras
+from keras.datasets import imdb
+from keras.models import Sequential
+
+
+
