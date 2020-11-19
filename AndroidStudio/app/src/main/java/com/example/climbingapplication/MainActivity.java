6450fc67e9b3bcff14955ae7c5366d336bf1ece6
Alex Ingham <16adwi@queensu.ca>
2020-11-19 18:32:10 -0500

    Added Azure integration

--- AndroidStudio/app/src/main/java/com/example/climbingapplication/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/example/climbingapplication/MainActivity.java
@@ -4,15 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.microsoft.windowsazure.mobileservices.*;
+
+import java.net.MalformedURLException;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainPage";
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Make connection to AZURE
+        try {
+            MobileServiceClient mClient = new MobileServiceClient(
+                    "https://androiddata.azurewebsites.net", // Our site URL
+                    this);
+            AzureServiceAdapter.Initialize(this);
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
