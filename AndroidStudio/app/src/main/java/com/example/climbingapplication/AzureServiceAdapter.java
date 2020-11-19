509a829f933d96dfc5f5c963c9ab45e5e5203411
Alex Ingham <16adwi@queensu.ca>
2020-11-19 18:32:10 -0500

    Added Azure integration

---
+++ AndroidStudio/app/src/main/java/com/example/climbingapplication/AzureServiceAdapter.java
@@ -0,0 +1,37 @@
+
+package com.example.climbingapplication;
+import android.content.Context;
+import com.microsoft.windowsazure.mobileservices.*;
+
+import java.net.MalformedURLException;
+
+public class AzureServiceAdapter {
+    private String mMobileBackendUrl = "https://myappname.azurewebsites.net";
+    private Context mContext;
+    private MobileServiceClient mClient;
+    private static AzureServiceAdapter mInstance = null;
+
+    private AzureServiceAdapter(Context context) throws MalformedURLException {
+        mContext = context;
+        mClient = new MobileServiceClient(mMobileBackendUrl, mContext);
+    }
+
+    public static void Initialize(Context context) throws MalformedURLException {
+        if (mInstance == null) {
+            mInstance = new AzureServiceAdapter(context);
+        } else {
+            throw new IllegalStateException("AzureServiceAdapter is already initialized");
+        }
+    }
+
+    public static AzureServiceAdapter getInstance() {
+        if (mInstance == null) {
+            throw new IllegalStateException("AzureServiceAdapter is not initialized");
+        }
+        return mInstance;
+    }
+
+    public MobileServiceClient getClient() {
+        return mClient;
+    }
+}
