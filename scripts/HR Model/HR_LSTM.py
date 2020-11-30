31bef980067b2fe4f9dbe10f00795c0a91e2af2a
nicoleooi <nicoleooi@hotmail.com>
2020-11-30 17:11:12 -0500

    started testing

--- scripts/HR Model/HR_LSTM.py
+++ scripts/HR Model/HR_LSTM.py
@@ -42,6 +42,8 @@ def format_data():
     '''
     Formats data and returns the formatted arrays.
     '''
+    print("Formatting data...")
+    
     path = "../../converted_data/hr_only/Georgia*.csv"
     appended_data = []
 
@@ -101,11 +103,42 @@ def format_data():
     # num samples, num time stamps, num features
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
     x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    
+    print("Finished formatting data")
 
     return x_train, x_test, y_train, y_test
 
+def normalize_data():
+    '''Data Normalization'''
+    print("Starting data normalization...")
+    
+    x = pd.read_csv('dropped_HR_x.csv', header = 0)
+    x.drop(x.columns[0],inplace=True, axis=1)
+    y = pd.read_csv('dropped_HR_y.csv', header = 0)
+    y.drop(y.columns[0], inplace=True, axis=1)
+
+    from sklearn.preprocessing import MinMaxScaler
+    scaler = MinMaxScaler(feature_range=(0,1))
+
+    x = scaler.fit_transform(x)
+    y = scaler.fit_transform(y)
+
+    #train on one climb 
+    split = 5156
+    x_train, x_test = x[:-split], x[-split:]
+    y_train, y_test = y[:-split], y[-split:]
+
+    # num samples, num time stamps, num features
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    
+    print("Finished data normalization")
+    
+    return x_train, x_test, y_train, y_test
+    
 def train_model(model, x_train, x_test, y_train, y_test):
     '''Model checks and tools to stop overfitting'''
+    print("Formatting model")
     from keras.callbacks import ModelCheckpoint, EarlyStopping
     filepath = 'models/{epoch:02d}-{loss:.4f}-{val_loss:.4f}-{val_mae:.4f}-{val_mae:.4f}.hdf5'
     callback = [EarlyStopping(monitor = 'val_loss', patience = 50),
@@ -119,20 +152,47 @@ def train_model(model, x_train, x_test, y_train, y_test):
     #batch size = number of samples to work through before updating internal params
     return model
 
-def test_model(model):
+def test_model(model, x_train, x_test, y_train, y_test):
+    print("Starting testing...")
+    
     #the best model will be the LAST file in the /models folder
-    files = glob.glob("/models")
+    files = os.listdir("./models")
     best_model = files[-1]
     print(best_model)
-    model.load_weights(best_model)
+    model.load_weights("./models/"+best_model)
     
     import time
+    #USE A WHILE LOOP FOR REAL TIME PREDICTION
+    #TO UPDATE THE CSV FILES
+    from sklearn.preprocessing import MinMaxScaler
+    scaler = MinMaxScaler(feature_range=(0,1))
+    obj_x = scaler.fit(x)
+    obj_y = scaler.fit(y)
+
+    for i in range(0, x_test.shape[0]):
+        hr_summary = [] #forecasted hr
+        x_input = x_test[i, :, :]
+        x_input = np.reshape(x_input, (1, x_input.shape[0], 1)) #1 sample, time stamp is 0, then 1 feature
+        x_input = model.predict(x_input)
+        prediction = scaler.inverse_transform(x_input)
+        
+        y_input = y_test[i,:]
+        y_input = np.reshape(y_input, (1,1))
+        actual = scaler.inverse_transform(y_input)
+        
+        hr_summary.append(actual)
+        hr_summary.extend(prediction)
+        
+        df = pd.DataFrame(hr_summary)
+        df = df.T #put it into a single row
+        #df.to_csv("real_time_HR.csv", mode='a', header=False, index=False)
+        
     
-    
+    print("Finished testing")
     return
 
 def main():
-    x_train, x_test, y_train, y_test = format_data()
+    x_train, x_test, y_train, y_test = normalize_data()
     
     '''Network Architecture'''
     model = Sequential()
@@ -147,7 +207,7 @@ def main():
     
     #model = train_model(model, x_train, x_test, y_train, y_test)
     
-    test_model(model)
+    test_model(model, x_train, x_test, y_train, y_test)
     #model.load_weights()
     
 if __name__ == "__main__":
